1801880d56e5cd60caf49fff99b91c36f9f409ad
alexw-gsct <56290422+alexw-gsct@users.noreply.github.com>
2020-11-26 09:52:49 +1100

    Update IRFinder.cpp

--- src/IRFinder.cpp
+++ src/IRFinder.cpp
@@ -414,14 +414,14 @@ int IRF_main_multithreaded(std::string reference_file, StringVector bam_files, S
 int IRF_main_multithreaded(std::string reference_file, StringVector bam_files, StringVector output_files, int max_threads){
 	
 	int use_threads = 0;
-	if(max_threads > 0 && max_threads <= omp_get_max_threads()) {
+	if(max_threads > 0 && max_threads <= omp_get_thread_limit()) {
 		if(max_threads > bam_files.size()) {
 			use_threads = bam_files.size();
 		} else {
 			use_threads = max_threads;
 		}
 	} else {
-		use_threads = omp_get_max_threads();
+		use_threads = omp_get_thread_limit();
 		if(use_threads < 1) {
 			use_threads = 1;
 		}
